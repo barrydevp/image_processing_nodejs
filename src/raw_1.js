8971b9e4cc1fe3fdb3c0b9b2206b08faf39e2b7b
barry <barrydevp@gmail.com>
2020-09-06 16:22:37 +0700

    fully gen :v

--- src/raw_1.js
+++ src/raw_1.js
@@ -4,9 +4,10 @@ const marchingSquares = require('../sticker/marching-squares')
 const svgb = require('svgb')
 
 const template = path.join(__dirname, '../assets/method-draw-image.svg')
-const inputImage = path.join(__dirname, '../assets/118614881_259104891753524_11646380289130353_n_1.png')
+const inputImage = path.join(__dirname, '../assets/118614881_259104891753524_11646380289130353_n.png')
 
-const strokeSize = 50
+let strokeSize = 80
+let color = '#A9A9A9'
 
 let width = 0
 let height = 0
@@ -33,7 +34,7 @@ const makeSvg = async (contours, width = 2400, height = 3600) => {
     const [resultWidth, resultHeight] = [width, height].map((val) => val + strokeSize)
     console.log(resultWidth, resultHeight)
 
-    const buffer = await svgb.toBuffer(template, {linePath: str, width: resultWidth, height: resultHeight, strokeSize, })
+    const buffer = await svgb.toBuffer(template, {linePath: str, width: resultWidth, height: resultHeight, strokeSize, color})
 
     await sharp(buffer).png().toFile('raw-svg.png')
 
@@ -49,7 +50,6 @@ const getStrokeBuffer = async (buffer) => {
     // }
     // const width = 2400
     // const height = 3600
-    const {width, height} = await sharp(inputImage).metadata()
 
     const dataLen = data.length
     console.log(dataLen)
@@ -83,10 +83,10 @@ const getStrokeBuffer = async (buffer) => {
     return strokeBuffer
 }
 
-const getNextBuffer = async (strokeBuffers) => {
+const getNextBuffer = async (strokeBuffers, buffer) => {
     if (!strokeBuffers.length) return false
 
-    const negateBuffer = await sharp(inputImage).negate().toBuffer()
+    const negateBuffer = await sharp(buffer).negate().toBuffer()
 
     const nextBuffer = await sharp(strokeBuffers[0]).composite([
         ...strokeBuffers.map(stroke => ({input: stroke, top: 0, left: 0})),
@@ -107,75 +107,96 @@ const getNextBuffer = async (strokeBuffers) => {
     return afterNegateBuffer
 }
 
-const getListStrokeBuffer = async (listStroke = []) => {
+const getListStrokeBuffer = async (listStroke = [], buffer) => {
 
     if (!listStroke.length) return []
 
-    const newObjectBuffer = await getNextBuffer(listStroke)
+    const newObjectBuffer = await getNextBuffer(listStroke, buffer)
 
     const nextStrokeBuffer = await getStrokeBuffer(newObjectBuffer)
 
     if (!nextStrokeBuffer) return listStroke
 
-    return getListStrokeBuffer(listStroke.concat([nextStrokeBuffer]))
+    return getListStrokeBuffer(listStroke.concat([nextStrokeBuffer]), buffer)
+}
+
+const gen = async (buffer) => {
+
+    console.log('in gen')
+
+    const {width: _w, height: _h} = await sharp(buffer).metadata()
+
+    width = _w
+    height = _h
+
+    const rawBuffer = await sharp(buffer).raw().toBuffer()
+
+    const data = [...rawBuffer]
+
+    const dataLen = data.length
+    console.log(dataLen)
+
+    console.log(data[5044812])
+
+    const isInside = (x, y) => {
+        return x >= 0 && y >= 0 && x < width && y < height
+            ? data[(y * width + x) * 4 + 3] > 0
+            : false
+    }
+
+    let contours = []
+    let startPos = -1
+    for (let i = 3; i < data.length; i += 4) {
+        if (data[i] > 0) {
+            startPos = (i + 1) / 4
+            break
+        }
+    }
+    console.log('startPos', startPos)
+    if (startPos >= 0) {
+        contours = marchingSquares(startPos % width, Math.floor(startPos / width), isInside)
+
+        // console.log(contours)
+    }
+
+    console.log('make svg, ', width, height)
+
+    const strokeBuffer = await makeSvg(contours, width, height)
+
+    const [resultWidth, resultHeight] = [width, height].map((val) => val + strokeSize)
+
+    console.log(resultWidth, resultHeight)
+
+    const listStroke = await getListStrokeBuffer([strokeBuffer], buffer)
+
+    const background = await sharp(strokeBuffer).composite([
+        ...listStroke.slice(1).map(stroke => ({input: stroke, top: 0, left: 0})),
+        {input: buffer, top: strokeSize / 2, left: strokeSize / 2},
+    ]).toBuffer()
+
+    return background
 }
 
 setImmediate(async () => {
     try {
         console.log('alo')
-        const buffer = await sharp(inputImage).raw().toBuffer()
+        const buffer = await sharp(inputImage).toBuffer()
         // console.log([...buffer])
+        //
 
-        const data = [...buffer]
+        color = '#fff'
+        strokeSize = 100
 
-        // for(let i = 10; i < 100; i++) {
-        //     data[i] = 255
-        //     console.log(data[i])
-        // }
-        // const width = 2400
-        // const height = 3600
-        const {width: _width, height: _height} = await sharp(inputImage).metadata()
+        const background = await gen(buffer)
 
-        width = _width
-        height = _height
+        await sharp(background).png().toFile('raw_gen.png')
 
-        const dataLen = data.length
-        console.log(dataLen)
+        color = '#A9A9A9'
+        strokeSize = 30
 
-        console.log(data[5044812])
+        const result = await gen(background)
 
-        const isInside = (x, y) => {
-            return x >= 0 && y >= 0 && x < width && y < height
-                ? data[(y * width + x) * 4 - 1] > 0
-                : false
-        }
-
-        let contours = []
-        let startPos = -1
-        for (let i = 3; i < data.length; i += 4) {
-            if (data[i] > 0) {
-                startPos = (i + 1) / 4
-                break
-            }
-        }
-        if (startPos >= 0) {
-            contours = marchingSquares(startPos % width, Math.floor(startPos / width), isInside)
-
-            // console.log(contours)
-        }
-
-        const strokeBuffer = await makeSvg(contours, width, height)
-
-        const [resultWidth, resultHeight] = [width, height].map((val) => val + strokeSize)
-
-        console.log(resultWidth, resultHeight)
-
-        const listStroke = await getListStrokeBuffer([strokeBuffer])
-
-        await sharp(strokeBuffer).composite([
-            ...listStroke.slice(1).map(stroke => ({input: stroke, top: 0, left: 0})),
-            {input: inputImage, top: strokeSize / 2, left: strokeSize / 2},
-        ]).png().toFile('combine-4.png')
+        await sharp(result).png().toFile('raw_result.png')
 
         // const nextBuffer = await sharp({
         //     create: {
