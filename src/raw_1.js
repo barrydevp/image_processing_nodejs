c4a2949004fdc9d33b1262502ee6409480bdc020
barry <barrydevp@gmail.com>
2020-09-06 17:06:29 +0700

    update smooth border

--- src/raw_1.js
+++ src/raw_1.js
@@ -4,13 +4,91 @@ const marchingSquares = require('../sticker/marching-squares')
 const svgb = require('svgb')
 
 const template = path.join(__dirname, '../assets/method-draw-image.svg')
-const inputImage = path.join(__dirname, '../assets/118614881_259104891753524_11646380289130353_n.png')
+// const nameInput = '118614881_259104891753524_11646380289130353_n'
+const nameInput = 'goav'
+const inputImage = path.join(__dirname, `../assets/${nameInput}.png`)
 
-let strokeSize = 80
+let strokeSize = 140
 let color = '#A9A9A9'
 
 let width = 0
 let height = 0
+
+// Properties of a line 
+// I:  - pointA (array) [x,y]: coordinates
+//     - pointB (array) [x,y]: coordinates
+// O:  - (object) { length: l, angle: a }: properties of the line
+const line = (pointA, pointB) => {
+    const lengthX = pointB.x - pointA.x
+    const lengthY = pointB.y - pointA.y
+    return {
+        length: Math.sqrt(Math.pow(lengthX, 2) + Math.pow(lengthY, 2)),
+        angle: Math.atan2(lengthY, lengthX)
+    }
+}
+
+// Position of a control point 
+// I:  - current (array) [x, y]: current point coordinates
+//     - previous (array) [x, y]: previous point coordinates
+//     - next (array) [x, y]: next point coordinates
+//     - reverse (boolean, optional): sets the direction
+// O:  - (array) [x,y]: a tuple of coordinates
+const controlPoint = (current, previous, next, reverse) => {
+    // When 'current' is the first or last point of the array
+    // 'previous' or 'next' don't exist.
+    // Replace with 'current'
+    const p = previous || current
+    const n = next || current
+    // The smoothing ratio
+    const smoothing = 0.2
+    // Properties of the opposed-line
+    const o = line(p, n)
+    // If is end-control-point, add PI to the angle to go backward
+    const angle = o.angle + (reverse ? Math.PI : 0)
+    const length = o.length * smoothing
+    // The control point position is relative to the current point
+    const x = current.x + Math.cos(angle) * length
+    const y = current.y + Math.sin(angle) * length
+    return {x, y}
+}
+
+// Create the bezier curve command 
+// I:  - point (array) [x,y]: current point coordinates
+//     - i (integer): index of 'point' in the array 'a'
+//     - a (array): complete array of points coordinates
+// O:  - (string) 'C x2,y2 x1,y1 x,y': SVG cubic bezier C command
+const bezierCommand = (point, i, a) => {
+    // start control point
+    const {x: cpsX, y: cpsY} = controlPoint(a[i - 1], a[i - 2], point)
+    // end control point
+    const {x: cpeX, y: cpeY} = controlPoint(point, a[i - 1], a[i + 1], true)
+    return `C ${cpsX},${cpsY} ${cpeX},${cpeY} ${point.x},${point.y}`
+}
+
+// Render the svg <path> element 
+// I:  - points (array): points coordinates
+//     - command (function)
+//       I:  - point (array) [x,y]: current point coordinates
+//           - i (integer): index of 'point' in the array 'a'
+//           - a (array): complete array of points coordinates
+//       O:  - (string) a svg path command
+// O:  - (string): a Svg <path> element
+const svgPath = (points, command) => {
+    // build the d attributes by looping over the points
+    const d = points.reduce((acc, point, i, a) => {
+
+        const {x, y} = point
+
+        return i === 0
+            // if first point
+            ? `M ${x},${y}`
+            // else
+            : `${acc} ${command(point, i, a)}`
+    }, '')
+
+    return `${d} Z`
+}
+
 
 const makeSvg = async (contours, width = 2400, height = 3600) => {
 
@@ -19,17 +97,26 @@ const makeSvg = async (contours, width = 2400, height = 3600) => {
 
 
     let startX = x + contours[0].x
-    let startY = y + contours[1].y
-    let str = `M${startX} ${startY}`
-    for (let i = 1; i < contours.length; i++) {
-        const endX = x + contours[i].x
-        const endY = y + contours[i].y
+    let startY = y + contours[0].y
+    let str = `M ${startX},${startY}`
+    str = svgPath(contours.map((point, i) => {
+        if (i === 0) return
+        if (i % 50 !== 0) return
 
-        str += ` L${endX} ${endY}`
-        startX = endX
-        startY = endY
-    }
-    str += ' Z'
+        const endX = x + point.x
+        const endY = y + point.y
+
+        return {x: endX, y: endY}
+    }).filter(Boolean), bezierCommand)
+
+    // console.log(str)
+    // for (let i = 1; i < contours.length; i++) {
+
+    //     str += ` L ${endX},${endY}`
+    //     startX = endX
+    //     startY = endY
+    // }
+    // str += ' Z'
 
     const [resultWidth, resultHeight] = [width, height].map((val) => val + strokeSize)
     console.log(resultWidth, resultHeight)
@@ -185,18 +272,18 @@ setImmediate(async () => {
         //
 
         color = '#fff'
-        strokeSize = 100
+        strokeSize = 150
 
         const background = await gen(buffer)
 
-        await sharp(background).png().toFile('raw_gen.png')
+        await sharp(background).png().toFile(`${nameInput}_gen.png`)
 
         color = '#A9A9A9'
         strokeSize = 30
 
         const result = await gen(background)
 
-        await sharp(result).png().toFile('raw_result.png')
+        await sharp(result).png().toFile(`${nameInput}_result.png`)
 
         // const nextBuffer = await sharp({
         //     create: {
