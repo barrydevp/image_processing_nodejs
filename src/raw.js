066310077a17cab76481a6c8efb9cc5550b32b66
barry <barrydevp@gmail.com>
2020-09-06 14:59:09 +0700

    new gen sticker

--- src/raw.js
+++ src/raw.js
@@ -24,6 +24,7 @@ const makeSvg = async (contours, width = 2400, height = 3600) => {
         startX = endX
         startY = endY
     }
+    str += 'Z'
 
     const [resultWidth, resultHeight] = [width, height].map((val) => val + strokeSize)
 
@@ -35,54 +36,54 @@ const makeSvg = async (contours, width = 2400, height = 3600) => {
 }
 
 setImmediate(async () => {
-        try {
-            console.log('alo')
-            const buffer = await sharp(path.join(__dirname, '../assets/118614881_259104891753524_11646380289130353_n.png')).raw().toBuffer()
-            // console.log([...buffer])
+    try {
+        console.log('alo')
+        const buffer = await sharp(path.join(__dirname, '../assets/118614881_259104891753524_11646380289130353_n.png')).raw().toBuffer()
+        // console.log([...buffer])
 
-            const data = [...buffer]
+        const data = [...buffer]
 
-            // for(let i = 10; i < 100; i++) {
-            //     data[i] = 255
-            //     console.log(data[i])
-            // }
-            const width = 2400
-            const height = 3600
+        // for(let i = 10; i < 100; i++) {
+        //     data[i] = 255
+        //     console.log(data[i])
+        // }
+        const width = 2400
+        const height = 3600
 
-            const dataLen = data.length
-            console.log(dataLen)
+        const dataLen = data.length
+        console.log(dataLen)
 
-            console.log(data[5044812])
+        console.log(data[5044812])
 
-            const isInside = (x, y) => {
-                return x >= 0 && y >= 0 && x < width && y < height
-                    ? data[(y * width + x) * 4 - 1] > 100
-                    : false
-            }
-
-            let contours = []
-            let startPos = -1
-            for (let i = 3; i < data.length; i += 4) {
-                if (data[i] > 100) {
-                    startPos = (i + 1) / 4
-                    break
-                }
-            }
-            if (startPos >= 0) {
-                contours = marchingSquares(startPos % width, Math.floor(startPos / width), isInside)
-
-                // console.log(contours)
-            }
-
-            const strokeBuffer = await makeSvg(contours)
-
-            await sharp(strokeBuffer).composite([
-                {input: path.join(__dirname, '../assets/118614881_259104891753524_11646380289130353_n.png'), top: strokeSize / 2, left: strokeSize / 2}
-            ]).png().toFile('combine-1.png')
-
-        } catch (e) {
-            console.log(e)
-
+        const isInside = (x, y) => {
+            return x >= 0 && y >= 0 && x < width && y < height
+                ? data[(y * width + x) * 4 - 1] > 0
+                : false
         }
+
+        let contours = []
+        let startPos = -1
+        for (let i = 3; i < data.length; i += 4) {
+            if (data[i] > 100) {
+                startPos = (i + 1) / 4
+                break
+            }
+        }
+        if (startPos >= 0) {
+            contours = marchingSquares(startPos % width, Math.floor(startPos / width), isInside)
+
+            // console.log(contours)
+        }
+
+        const strokeBuffer = await makeSvg(contours)
+
+        await sharp(strokeBuffer).composite([
+            {input: path.join(__dirname, '../assets/118614881_259104891753524_11646380289130353_n.png'), top: strokeSize / 2, left: strokeSize / 2}
+        ]).png().toFile('combine-1.png')
+
+    } catch (e) {
+        console.log(e)
+
     }
+}
 )
